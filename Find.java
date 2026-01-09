4071ab5ce819ec823fe23d9e7b7d232ea37a6058
ksaini-web <kartiksaini9084@gmail.com>
2026-01-09 18:36:33 +0530

    Implement method to find first and last occurrence of a character by Using Recursion

---
+++ Find.java
@@ -0,0 +1,37 @@
+
+public class find {
+    
+    public static int first =-1;
+public static int last = -1;
+
+public static void  FindOccurance(String str,int idx,char element){
+
+    if(idx==str.length()){
+
+       System.out.println("The First Occurance of Element is :"+first);
+       System.out.println("The Last Occurance of Element is :"+last);
+        return ;
+    }
+
+    char CurrElement=str.charAt(idx);
+
+    if(CurrElement==element){
+
+        if(first==-1){
+            first=idx;
+        }
+        else{
+            last=idx;
+        }
+    }
+    FindOccurance(str,idx+1,element);
+}
+
+
+    public static void main(String[]args){
+String str="abacaghga";
+
+       FindOccurance(str,0,'a');
+    }
+    
+}
